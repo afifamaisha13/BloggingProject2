fe7652d5d8b92bcbe9aec4cb8fb457719a9ae7a3
afifamaisha13 <afifamaisha13@gmail.com>
2026-04-23 08:06:27 +0600

    ok

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,31 @@
-const mysql = require("mysql2");
+/*const mysql = require("mysql2");
 
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "MySQL",   // add password if you have
     database: "blog_db1"
+});
+
+db.connect((err) => {
+    if (err) {
+        console.log("DB Error:", err);
+    } else {
+        console.log("MySQL Connected ✅");
+    }
+});
+
+module.exports = db;*/
+
+
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,12 @@ app.get("/", (req, res) => {
     res.send("Server + DB running 🚀");
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+//app.listen(3000, () => {
+    //console.log("Server running on port 3000");
+//});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
